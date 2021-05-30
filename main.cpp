2772d0af1728d643252ec6aa3a92eae687080f4a
yarin177 <51924888+yarin177@users.noreply.github.com>
2021-05-31 02:16:41 +0300

    Update main.cpp
    Changed Width and Height to the window and added a color spectrum next to it

--- main.cpp
+++ main.cpp
@@ -6,8 +6,8 @@
 #include <iostream>
 #include <cmath>
 #include "fft.h"
-
-
+#include <algorithm>
+#include <vector>
 using namespace std;
 
 const int N = 4096; // samples
@@ -17,22 +17,59 @@ double magnitude[N];
 double dBm[N / 64][N / 64]; // used to convert FFT output to dBm
 double spectrums[N / 64][N / 64];
 
+vector<double> f(double x) {
+    x = 3 * x;
+    double R = min(x, 1.0);
+    double G = min(1.0, max(x - 1, 0.0));
+    double B = min(1.0, max(x - 2, 0.0));
+    vector<double> color = { R,G,B };
+    return color;
+}
 void displaySpectrogram() {
 
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    glOrtho(0, 64, 0, 64, -1, 1); // project a 64x64 matrix on a 32x32 window for better resolution
-    glPointSize(5);
+    glOrtho(0, 72, 0, 64, -1, 1); // project a 64x64 matrix on a 640x640 window for better overview
+    glPointSize(10); // because the window size is x10 bigger, we'll enlarge each point by x10
+    double main_color = 0, R = 0, G = 0, B = 0;
 
     glClear(GL_COLOR_BUFFER_BIT); // Clear the color buffer (background)
     glBegin(GL_POINTS);
+    double mat[62][11];
+    double x = 0;
+
+    for (int i = 0; i < 50; ++i) {
+        for (int j = 0; j < 11; ++j) {
+            mat[i][j] = x;
+        }
+        x += 1 / 50.0;
+    }
+    int counter = 0;
     for (int i = 0; i < 64; i++) // go over the array of colors
     {
-        for (int j = 0; j < 64; j++)
+        for (int j = 0; j < 72; j++)
         {
-            glColor3f(dBm[i][j], dBm[i][j], dBm[i][j]);
-            glVertex2f(j, i);
+            if (j >= 64 && i < 50)
+            {
+                vector<double> colors = f(mat[i][counter]);
+                glColor3f(colors[0], colors[1], colors[2]);
+                glVertex2f(j, i);
+                counter++;
+            }
+            else if (j >= 64 && i > 50)
+            {
+                glColor3f(0, 0, 0);
+                glVertex2f(j, i);
+                counter++;
+            }
+            else
+            {
+                vector<double> colors = f(dBm[i][j]);
+                glColor3f(colors[0], colors[1], colors[2]);
+                glVertex2f(j, i);
+            }
         }
+        counter = 0;
     }
     glEnd();
 
@@ -91,8 +128,8 @@ int main(int argc, char** argv)
 
     // GUI Stuff
     glutInit(&argc, argv);                 // Initialize GLUT
-    glutCreateWindow("32z32 spectrogram"); // Create a window with the given title
-    glutInitWindowSize(32, 32);   // Set the window's initial width & height
+    glutInitWindowSize(720, 640);   // Set the window's initial width & height
+    glutCreateWindow("640x640 spectrogram (based on 64x64 matrix)"); // Create a window with the given title
     glutDisplayFunc(displaySpectrogram); // Register display callback handler for window re-paint
     glutMainLoop();           // Enter the event-processing loop
     return 0;
