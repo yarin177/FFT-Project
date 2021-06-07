b8691369d61ce05788411ecb5682f196be2204d4
yarin177 <51924888+yarin177@users.noreply.github.com>
2021-06-07 19:34:03 +0300

    Moved data to Heap & Cleaner code
    - Now you can control the window size (based on the samples obviously)
    - Made Hanning Window a separate function

--- main.cpp
+++ main.cpp
@@ -5,11 +5,13 @@
 #include <cmath>
 #include "fft.h"
 #include <algorithm>
-
+#include <vector>
 using namespace std;
 
-const int N = 4096; // Samples
-double dBm[128][32]; // Used to hold dBm values
+const int N = 204800; // Samples
+const int TIME_RESOLUTION = 400;
+const int FREQUENCY_RESOLUTION = 512;
+vector< vector<double> > dBm(TIME_RESOLUTION, std::vector<double>(FREQUENCY_RESOLUTION, 0));
 
 struct Color { // Represents a pixel color
     double R;
@@ -17,20 +19,22 @@ struct Color { // Represents a pixel color
     double B;
 };
 
-void GetColorEntry(Color& color, double x) { // Converts dBm entry to RGB color entry
+Color GetColorEntry(double x) { // Converts dBm entry to RGB color entry
+    Color pixel;
     x = 3 * x;
     double R = min(x, 1.0);
     double G = min(1.0, max(x - 1, 0.0));
     double B = min(1.0, max(x - 2, 0.0));
-    color = { R,G,B };
+    pixel = { R,G,B };
+    return pixel;
 }
 
 void displaySpectrogram() {
 
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    glOrtho(0, 32, 0, 128, -1, 1); // Project a 64x64 matrix on a 640x640 window for better overview
-    glPointSize(10); // Because the window size is x10 bigger, we'll enlarge each point by x10
+    glOrtho(0, FREQUENCY_RESOLUTION, 0, TIME_RESOLUTION, -1, 1); // Project a 64x64 matrix on a 640x640 window for better overview
+    //glPointSize(10); // Because the window size is x10 bigger, we'll enlarge each point by x10
     double R = 0, G = 0, B = 0;
 
     glClear(GL_COLOR_BUFFER_BIT); // Clear the color buffer (background)
@@ -38,11 +42,11 @@ void displaySpectrogram() {
     double x = 0;
     Color colors;
 
-    for (int i = 0; i < 128; i++) // go over the array of colors
+    for (int i = 0; i < TIME_RESOLUTION; i++) // go over the array of colors
     {
-        for (int j = 0; j < 32; j++)
+        for (int j = 0; j < FREQUENCY_RESOLUTION; j++)
         {
-            GetColorEntry(colors,dBm[i][j]);
+            colors = GetColorEntry(dBm[i][j]);
             glColor3f(colors.R, colors.G, colors.B);
             glVertex2f(j, i);
         }
@@ -52,26 +56,37 @@ void displaySpectrogram() {
     glFlush();  // Render now
 }
 
-Complex ApplyHanningWindow(Complex in[], int i, int chuck_size)
+Complex ApplyHanningWindow(vector<Complex> &in, int i, int chuck_size)
 {
     double multiplier = 0.5 * (1 - cos(2 * M_PI * i / (chuck_size)));//Hanning Window
-    return multiplier * in[i];
+    return in[i] * multiplier;
+}
+
+void NormalizedBm(double max_dBm, vector<double> magnitude)
+{
+    int counter = 0;
+    for (int i = 0; i < TIME_RESOLUTION; i++)
+    {
+        for (int j = 0; j < FREQUENCY_RESOLUTION; j++)
+        {
+            dBm[i][j] = (30 + (20 * log10(magnitude[counter]))) / max_dBm; // Normalize output
+            counter++;
+        }
+    }
 }
 
 int main(int argc, char** argv)
 {
-    Complex in[N]; // Hold the samples (Complex sine wave)
-    double t[N]; // Time vector 
-    double magnitude[N];
-    double spectrums[128][32];
+    vector<Complex> in(N); // Hold the samples (Complex sine wave)
+    vector<double> t(N); // Time vector 
+    vector<double> magnitude(N);
 
-    int frequency_resolution = 32, time_resolution = 128;
 
     const double Fs = 100; // How many time points are needed i,e., Sampling Frequency
     const double  T = 1 / Fs; // At what intervals time points are sampled
     double f = 4; // Frequency
-    int chuck_size = 32; // Chunk size (N / 64=64 chunks)
-    Complex chuck[32];
+    int chuck_size = FREQUENCY_RESOLUTION; // Chunk size (N / 64=64 chunks)
+    Complex chuck[FREQUENCY_RESOLUTION];
     int j = 0;
     int counter = 0;
     double max_dBm = 0.0;
@@ -81,19 +96,19 @@ int main(int argc, char** argv)
         t[i] = i * T;
         in[i] = { (0.7 * cos(2 * M_PI * f * t[i])), (0.7 * sin(2 * M_PI * f * t[i])) };// generate (complex) sine waveform
 
-        in[i] = ApplyHanningWindow(in, i, 32);
+        in[i] = ApplyHanningWindow(in, i, FREQUENCY_RESOLUTION);
 
         chuck[j] = in[i]; // take every value and put it in chunk until 64
 
         //compute FFT for each chunk
-        if (i + 1 == chuck_size) // for each set of 64 chunks (i.e. 64,128,192), apply FFT and save it all in a 1d array (magnitude)
+        if (i + 1 == chuck_size) // for each set of 64 chunks (i.e. 64,TIME_RESOLUTION,192), apply FFT and save it all in a 1d array (magnitude)
         {
-            f = f + 0.7;
-            chuck_size += 32;
-            CArray data(chuck, 32); // Apply fft for 64 chunk
+            //f = f + 0.235;
+            chuck_size += FREQUENCY_RESOLUTION;
+            CArray data(chuck, FREQUENCY_RESOLUTION); // Apply fft for 64 chunk
             fft(data);
             j = 0;
-            for (int h = 0; h < 32; h++)
+            for (int h = 0; h < FREQUENCY_RESOLUTION; h++)
             {
                 magnitude[counter] = abs(data[h]); // save it all in 1d array (we switch it to 2d later on)
 
@@ -107,22 +122,12 @@ int main(int argc, char** argv)
         else
             j++;
     }
-    counter = 0; 
-    // We want to save the magnitude into a 2d array of 64x64 inorder to plot the spectrogram, for that we'll go over the array and save it to a new 2d array.
-    std::cout << "max dBm is: " << max_dBm;
-    for (int i = 0; i < 128; i++)
-    {
-        for (int j = 0; j < 32; j++)
-        {
-            dBm[i][j] = (30 + (20 * log10(magnitude[counter]))) / max_dBm; // Normalize output
-            counter++;
-        }
-    }
+    NormalizedBm(max_dBm, magnitude);
 
     // GUI Stuff
     glutInit(&argc, argv);                 // Initialize GLUT
-    glutInitWindowSize(320, 1280);   // Set the window's initial width & height
-    glutCreateWindow("1280x320 spectrogram (based on 64x64 matrix)"); // Create a window with the given title
+    glutInitWindowSize(FREQUENCY_RESOLUTION, TIME_RESOLUTION);   // Set the window's initial TIME_RESOLUTION & FREQUENCY_RESOLUTION
+    glutCreateWindow("spectrogram"); // Create a window with the given title
     glutDisplayFunc(displaySpectrogram); // Register display callback handler for window re-paint
     glutMainLoop();           // Enter the event-processing loop
     return 0;
