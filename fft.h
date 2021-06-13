8ea70bdfd3dc0d8670b54fbe0586434edcccc46b
yarin177 <51924888+yarin177@users.noreply.github.com>
2021-06-13 23:11:49 +0300

    Changed complex<double> to complex<float>

--- fft.h
+++ fft.h
@@ -2,7 +2,7 @@
 #include <complex>
 #include <valarray>
 
-typedef std::complex<double> Complex;
+typedef std::complex<float> Complex;
 typedef std::valarray<Complex> CArray;
 
 void fft(CArray& x)
@@ -10,18 +10,17 @@ void fft(CArray& x)
     const size_t N = x.size();
     if (N <= 1) return;
 
-    // divide
-    CArray even = x[std::slice(0, N / 2, 2)];
+    CArray even = x[std::slice(0, N / 2, 2)]; //split the inputs in even / odd indices subarrays
     CArray  odd = x[std::slice(1, N / 2, 2)];
 
     // conquer
-    fft(even);
+    fft(even); 
     fft(odd);
 
     // combine
     for (size_t k = 0; k < N / 2; ++k)
     {
-        Complex t = std::polar(1.0, -2 * M_PI * k / N) * odd[k];
+        Complex t = std::polar<float>(1.0, -2 * M_PI * k / N) * odd[k];
         x[k] = even[k] + t;
         x[k + N / 2] = even[k] - t;
     }
