a68081da7664ecacd3dd03df00621f314234b4e8
yarin177 <51924888+yarin177@users.noreply.github.com>
2021-09-07 15:46:37 +0300

    Now the FFT takes a complex vector instead of valarray

--- fft.hpp
+++ fft.hpp
@@ -1,26 +1,49 @@
 #define _USE_MATH_DEFINES
+#pragma once
+#include <cmath>
+#include <vector>
 #include <complex>
 #include <valarray>
 
-typedef std::complex<float> Complex;
-typedef std::valarray<Complex> CArray;
 
-void fft(CArray& x)
+using std::vector;
+using std::complex;
+ 
+vector<complex<float>> slicing(vector<complex<float>> vec, unsigned int X, unsigned int Y, unsigned int stride)
 {
+    // To store the sliced vector
+    vector<complex<float>> result;
+ 
+    // Copy vector using copy function()
+    int i = X;
+    while (result.size() < Y)
+    {
+        result.push_back(vec[i]);
+        i = i + stride;
+    }
+    // Return the final sliced vector
+    return result;
+}
+ 
+void fft(vector<complex<float>>& x)
+{
+    // Check if it is splitted enough
+ 
     const size_t N = x.size();
-    if (N <= 1) return;
-
-    CArray even = x[std::slice(0, N / 2, 2)]; //split the inputs in even / odd indices subarrays
-    CArray  odd = x[std::slice(1, N / 2, 2)];
-
+    if (N <= 1) 
+        return;
+ 
+    vector<complex<float>> even = slicing(x, 0, N / 2,2); //split the inputs in even / odd indices subarrays
+    vector<complex<float>>  odd = slicing(x, 1, N / 2,2);
+ 
     // conquer
-    fft(even); 
+    fft(even);
     fft(odd);
-
+ 
     // combine
     for (size_t k = 0; k < N / 2; ++k)
     {
-        Complex t = std::polar<float>(1.0, -2 * M_PI * k / N) * odd[k];
+        complex<float> t = std::polar<float>(1.0, -2 * M_PI * k / N) * odd[k];
         x[k] = even[k] + t;
         x[k + N / 2] = even[k] - t;
     }
